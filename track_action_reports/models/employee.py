31c5a655cbaeb24e240bbf5858cd2a912bdcfb85
mfathi1995 <mahmoud.ali@intalio.com>
2024-12-25 12:31:24 +0200

    18 t0 17

--- track_action_reports/models/employee.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, api, fields
-
-
-class EmployeeInherit(models.Model):
-    _inherit = 'hr.employee'
-
-    test = fields.Char(string="Test", required=False, )
-    res_users = fields.Many2one('res.users', 'Users', ondelete='cascade')
