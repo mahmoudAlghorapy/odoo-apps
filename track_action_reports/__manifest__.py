1abec47b62fe5dace4aebe1d3caef94584b376ea
mfathi1995 <mahmoud.ali@intalio.com>
2024-12-25 10:59:06 +0200

    18 t0 17

--- track_action_reports/__manifest__.py
+++ track_action_reports/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Track Action Reports',
-    'version': '16.1',
+    'version': '17.1',
     'depends': ['base', 'hr','purchase','sale', 'account'],
     'author': 'Mahmoud Fathy ',
     'summary': "This module track the main print in odoo sale account purchase stock ",
