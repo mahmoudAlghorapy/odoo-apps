130f0f6d90b5c1d955cbd018cd597acfefad71cd
mfathi1995 <mahmoud.ali@intalio.com>
2025-01-14 14:01:36 +0200

    add [payment_deductions]

--- payment_deductions/__manifest__.py
+++ payment_deductions/__manifest__.py
@@ -5,7 +5,13 @@
 
     "license": "LGPL-3",
     "category": "account",
-    'price': '40',
+    'price': '50',
+ 'summary': "This module add many deduction in payment journal entry  ",
+    'description': """
+This module add many deduction in payment journal entry  when we press action confirm   after we make accounts configration in setting.
+
+
+    """,
     "depends": [
         "account", "account_accountant",
     ],
